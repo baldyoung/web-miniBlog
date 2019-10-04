8cf9b6178193ad1f5a8584b18361e9f7c098dd33
baldyoung <baldyoung@163.com>
2019-10-04 16:52:36 +0800

    1.添加了对项目默认访问路径的重定向

---
+++ src/MiniBlog/Main/Controller/Common/DefaultController.java
@@ -0,0 +1,18 @@
+package MiniBlog.Main.Controller.Common;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+@Controller
+@RequestMapping(value={"/"})
+public class DefaultController {
+
+    @RequestMapping(method={GET})
+    public String defaultMethod(){
+        return "redirect:/WebContext/MiniBlog_Front/index";
+    }
+}
