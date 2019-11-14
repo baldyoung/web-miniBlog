d5cebf0e4e7829fad56f924013d55d2ccce867d7
baldyoung <791178881@qq.com>
2019-11-14 18:28:58 +0800

    ...

---
+++ src/MiniBlog/Main/Controller/IndexDefaultController.java
@@ -0,0 +1,14 @@
+package MiniBlog.Main.Controller;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class IndexDefaultController {
+    @RequestMapping()
+    public String defaultMethod(){
+        return "redirect:/WebContext/MiniBlog_Front/X_Index/index";
+    }
+}
